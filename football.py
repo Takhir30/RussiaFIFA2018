9ed6723f126dd5e93343f4911d7ae0531aab6667
Takhir <baygeriev87@mail.ru>
2018-06-24 03:17:42 +0300

    added penalty function and kick_out function to manage some different statements in a game, wrote some comments

--- football.py
+++ football.py
@@ -55,13 +55,24 @@ groups = {
     }
 }
 
+final_grid = [['A1', 'B2'], ['C1', 'D2'], ['E1', 'F2'], ['G1', 'H2'],
+              ['B1', 'A2'], ['D1', 'C2'], ['F1', 'E2'], ['H1', 'G2']]
 
+
+# Random number of goals taking into account the dispersion
 def goals():
     goals = choice(range(0, 10), p=[0.37, 0.27, 0.2, 0.13, 0.021, 0.005,
                                     0.002, 0.0014, 0.0005, 0.0001])
     return goals
 
 
+# Random number of penalty goals taking into account the dispersion
+def penalty_goals():
+    goals = choice(range(0, 6), p=[0.01, 0.02, 0.07, 0.1, 0.4, 0.4])
+    return goals
+
+
+# The result of a single match
 def match(team_1, team_2):
     team_1_goals = goals()
     team_2_goals = goals()
@@ -70,6 +81,8 @@ def match(team_1, team_2):
             [team_2, team_2_goals, team_1_goals]]
 
 
+# Combinating all teams in a group, playing all matches in a group,
+# printing all results with some statistic and returning two best teams
 def group_results(group_name):
     group = groups[group_name]
     matches = [i for i in combinations(group.keys(), 2)]
@@ -79,7 +92,6 @@ def group_results(group_name):
     for teams in matches:
         result_of_match = match(teams[0], teams[1])
         statistics(result_of_match, group_name)
-
     print('<----------------------->\n')
     print(f'GROUP {group_name}')
     t = PrettyTable(['Country', 'games', 'won', 'draw', 'lost',
@@ -97,6 +109,7 @@ def group_results(group_name):
     return mini_group
 
 
+#Iterating trougth all groups and returning all winners
 def group_stage_games(groups):
     play_off_teams = {}
     for group in groups:
@@ -106,6 +119,7 @@ def group_stage_games(groups):
     return play_off_teams
 
 
+#Counting goals, points, etc to define a winners
 def statistics(result, group):
     for i in result:
         groups[group][i[0]]['games'] += 1
@@ -122,9 +136,69 @@ def statistics(result, group):
             groups[group][i[0]]['lost'] += 1
 
 
-def final_stage():
+# Penalty statements
+def penalties(team_1, team_2):
+    while True:
+        team_1_goals = penalty_goals()
+        team_2_goals = penalty_goals()
+        if team_1_goals != team_2_goals:
+            print(f'Penalty:    {team_1_goals}:{team_2_goals} --------------->')
+            if team_1_goals > team_2_goals:
+                return team_1 ,team_2
+            elif team_1_goals < team_2_goals:
+                return team_2, team_1
+
+
+# Overall statements to kick a team out
+def kick_out(result):
+    team_1 = result[0][0]
+    team_2 = result[1][0]
+    team_1_goals = result[0][1]
+    team_2_goals = result[1][1]
+    if team_1_goals != team_2_goals:
+        if team_1_goals > team_2_goals:
+            return team_1, team_2
+        elif team_1_goals < team_2_goals:
+            return team_2, team_1
+    else:
+        winner, loser = penalties(team_1, team_2)
+
+
+# Final stage games
+def final_stage(final_grid):
     play_off_teams = group_stage_games(groups)
+    winners = []
+    losers = []
+    if len(final_grid) * len(final_grid[0]) == 16:
+        print('\n1/8 final')
+        for teams in final_grid:
+            result = match(play_off_teams[teams[0]], play_off_teams[teams[1]])
+            winner, loser = kick_out(result)
+            winners.append(winner)
+    elif len(final_grid) == 8:
+        print('\n1/4 final')
+        for teams in range(0,len(final_grid),2):
+            result = match(final_grid[teams], final_grid[teams+1])
+            winner, loser = kick_out(result)
+            winners.append(winner)
+    elif len(final_grid) == 4:
+        print('\n1/2 final')
+        for teams in range(0,len(final_grid),2):
+            result = match(final_grid[teams], final_grid[teams+1])
+            winner, loser = kick_out(result)
+            winners.append(winner)
+            losers.append(loser)
+        print('\n3rd place game')
+        result = match(losers[0], losers[1])
+        third_place, loser = kick_out(result)
+    elif len(final_grid) == 2:
+        print('\nFinal')
+        result = match(final_grid[0], final_grid[1])
+        first_place, second_place = kick_out(result)
+        print(f"""The winner is {first_place}
+                Second place goes to {second_place}
+                Third place goes to {first_place}""")
+    final_stage(winners)
 
 
-
-final_stage()
+final_stage(final_grid)
