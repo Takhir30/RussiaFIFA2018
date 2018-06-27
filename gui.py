82842071443fb5cb07e208f0022ba0919b33a898
Takhir <baygeriev87@mail.ru>
2018-06-28 02:08:56 +0300

    added more match results in our gui, changed match func to get string with match result

--- gui.py
+++ gui.py
@@ -15,23 +15,29 @@ h = h - 150
 root.geometry(f'400x300+{w}+{h}')
 
 
+# Close app button
 def close_window():
     root.destroy()
 
 
+# Raise frame button
 def raise_frame(frame):
     frame.tkraise()
 
 
+# Results frame generator
 def results_show(frame, group):
+    trash, group_results, match_results = fb.group_results(group)
+    row = 0
+    for match in match_results:
+        tk.Label(frame, text=match).grid(row=row, column=0, columnspan=8)
+        row += 1
     text_title = ['Country', 'Games', 'Won', 'Draw', 'Lost', 'Gf', 'Ga', 'Diff', 'Points']
     column = 0
     for i in text_title:
-        tk.Label(frame, text=i).grid(row=0, column=column, columnspan=2)
+        tk.Label(frame, text=i).grid(row=7, column=column, columnspan=2)
         column += 2
-
-    group_results = fb.group_results(group)[1]
-    row = 1
+    row = 8
     for result in group_results:
         column = 0
         for attribute in result:
@@ -40,10 +46,12 @@ def results_show(frame, group):
             var.set(attribute)
             column += 2
         row += 1
-    tk.Button(frame, text='Back', width=20, height=2, command=lambda:raise_frame(group_stage)).grid(row=6, column=5, rowspan=2, columnspan=6)
+    tk.Button(frame, text='Back', width=20, height=2, command=lambda:raise_frame(group_stage)).grid(row=12, column=5, rowspan=2, columnspan=6)
 
+# WorldCup img
 img = tk.PhotoImage(file='254645_w.png')
 
+#All frames
 start_page = tk.Frame(root)
 second_page = tk.Frame(root)
 third_page = tk.Frame(root)
@@ -60,7 +68,7 @@ final_stage_page = tk.Frame(root)
 frames = [start_page, second_page, third_page, group_stage, group_A, group_B, group_C,
           group_D, group_E, group_F, group_G, group_H, final_stage_page]
 
-
+# Frame raisor
 for frame in frames:
     frame.grid(row=0, column=0, sticky='news')
 
@@ -79,7 +87,6 @@ tk.Label(third_page, bg='#27292d', image=img, width=400).pack()
 tk.Button(third_page, text='Group stage', width=20, height=2, command=lambda:raise_frame(group_stage)).pack()
 tk.Button(third_page, text='Final stage', width=20, height=2, command=lambda:raise_frame(final_stage_page)).pack()
 
-
 # Group stage page
 tk.Button(group_stage, text='Group A', width=10, height=2, command=lambda:raise_frame(group_A)).place(x=10, y=25)
 tk.Button(group_stage, text='Group B', width=10, height=2, command=lambda:raise_frame(group_B)).place(x=110, y=25)
@@ -89,16 +96,13 @@ tk.Button(group_stage, text='Group E', width=10, height=2, command=lambda:raise_
 tk.Button(group_stage, text='Group F', width=10, height=2, command=lambda:raise_frame(group_F)).place(x=110, y=100)
 tk.Button(group_stage, text='Group G', width=10, height=2, command=lambda:raise_frame(group_G)).place(x=210, y=100)
 tk.Button(group_stage, text='Group H', width=10, height=2, command=lambda:raise_frame(group_H)).place(x=310, y=100)
-
 tk.Button(group_stage, text='Back', width=20, height=2, command=lambda:raise_frame(third_page)).place(x=120, y=175)
 
 # Group A
 results_show(group_A, 'A')
 
-
 # Group B
 results_show(group_B, 'B')
-
 
 # Group C
 results_show(group_C, 'C')
@@ -120,5 +124,13 @@ results_show(group_H, 'H')
 
 
 # Final stage
+tk.Button(final_stage_page, text='1/8', width=10, height=2, command=lambda:raise_frame(group_A)).place(x=10, y=25)
+tk.Button(final_stage_page, text='1/4', width=10, height=2, command=lambda:raise_frame(group_B)).place(x=110, y=25)
+tk.Button(final_stage_page, text='1/2', width=10, height=2, command=lambda:raise_frame(group_C)).place(x=210, y=25)
+tk.Button(final_stage_page, text='3d place', width=10, height=2, command=lambda:raise_frame(group_D)).place(x=310, y=25)
+tk.Button(final_stage_page, text='Final', width=53, height=2, command=lambda:raise_frame(group_E)).place(x=10, y=100)
+tk.Button(final_stage_page, text='Back', width=20, height=2, command=lambda:raise_frame(third_page)).place(x=120, y=175)
+
+
 raise_frame(start_page)
 root.mainloop()
