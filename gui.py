081995a30fa266cd44f1ffdc4a4f48916803e4e2
Takhir <baygeriev87@mail.ru>
2018-06-29 15:50:36 +0300

    done all default mode

--- gui.py
+++ gui.py
@@ -2,8 +2,11 @@ import tkinter as tk
 import example as fb
 
 
+
 root = tk.Tk()
 
+group_results, match_results, all_final_matches = fb.main()
+print(all_final_matches)
 root.title('FIFA 2018')
 
 w = root.winfo_screenwidth() # ширина экрана
@@ -26,10 +29,10 @@ def raise_frame(frame):
 
 
 # Results frame generator
+groups_in = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 def results_show(frame, group):
-    trash, group_results, match_results = fb.group_results(group)
     row = 0
-    for match in match_results:
+    for match in match_results[groups_in[group]]:
         tk.Label(frame, text=match).grid(row=row, column=0, columnspan=8)
         row += 1
     text_title = ['Country', 'Games', 'Won', 'Draw', 'Lost', 'Gf', 'Ga', 'Diff', 'Points']
@@ -38,7 +41,7 @@ def results_show(frame, group):
         tk.Label(frame, text=i).grid(row=7, column=column, columnspan=2)
         column += 2
     row = 8
-    for result in group_results:
+    for result in group_results[groups_in[group]]:
         column = 0
         for attribute in result:
             var = tk.StringVar()
@@ -128,26 +131,36 @@ results_show(group_G, 'G')
 # Group H
 results_show(group_H, 'H')
 
-
 # Final stage
-tk.Button(final_stage_page, text='1/8', width=10, height=2, command=lambda:raise_frame(group_A)).place(x=10, y=25)
-tk.Button(final_stage_page, text='1/4', width=10, height=2, command=lambda:raise_frame(group_B)).place(x=110, y=25)
-tk.Button(final_stage_page, text='1/2', width=10, height=2, command=lambda:raise_frame(group_C)).place(x=210, y=25)
-tk.Button(final_stage_page, text='3d place', width=10, height=2, command=lambda:raise_frame(group_D)).place(x=310, y=25)
-tk.Button(final_stage_page, text='Final', width=53, height=2, command=lambda:raise_frame(group_E)).place(x=10, y=100)
+tk.Button(final_stage_page, text='1/8', width=10, height=2, command=lambda:raise_frame(one_eighth)).place(x=10, y=25)
+tk.Button(final_stage_page, text='1/4', width=10, height=2, command=lambda:raise_frame(quaterfinal)).place(x=110, y=25)
+tk.Button(final_stage_page, text='1/2', width=10, height=2, command=lambda:raise_frame(semifinal)).place(x=210, y=25)
+tk.Button(final_stage_page, text='3d place', width=10, height=2, command=lambda:raise_frame(third_place)).place(x=310, y=25)
+tk.Button(final_stage_page, text='Final', width=53, height=2, command=lambda:raise_frame(final)).place(x=10, y=100)
 tk.Button(final_stage_page, text='Back', width=20, height=2, command=lambda:raise_frame(third_page)).place(x=120, y=175)
 
 # 1/8
-one_eighth_matches = fb.final_stage(fb.final_grid, fb.play_off_teams)
-for match in one_eighth_matches:
+for match in all_final_matches[:8]:
     tk.Label(one_eighth, text=match).pack()
+tk.Button(one_eighth, text='Back', width=20, height=2, command=lambda:raise_frame(final_stage_page)).pack()
+
 # 1/4
+for match in all_final_matches[8:12]:
+    tk.Label(quaterfinal, text=match).pack()
+tk.Button(quaterfinal, text='Back', width=20, height=2, command=lambda:raise_frame(final_stage_page)).pack()
 
 # 1/2
+for match in all_final_matches[12:14]:
+    tk.Label(semifinal, text=match).pack()
+tk.Button(semifinal, text='Back', width=20, height=2, command=lambda:raise_frame(final_stage_page)).pack()
 
 # 3d place
+tk.Label(third_place, text=all_final_matches[-2]).pack()
+tk.Button(third_place, text='Back', width=20, height=2, command=lambda:raise_frame(final_stage_page)).pack()
 
 # Final
+tk.Label(final, text=all_final_matches[-1]).pack()
+tk.Button(final, text='Back', width=20, height=2, command=lambda:raise_frame(final_stage_page)).pack()
 
 raise_frame(start_page)
 root.mainloop()
